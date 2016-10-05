eb08c2bc4d7c2af03a0b2fa90c38cb94649a7374
PlavosVasilis <vplavos@gmail.com>
2016-10-05 20:29:15 +0300

    fixed!

--- python_exercises/exercises_02/Exercise 10.py
+++ python_exercises/exercises_02/Exercise 10.py
@@ -1,18 +1,43 @@
 ones = 0
 zeros = 0
+lr0 = 0
+lr1 = 0
+cr0 = 0
+cr1 = 0
 seq = input("Enter binary sequence: ")
 t = len(seq)
 for i in range (0, t):
     n = int(seq[i])
-    if n == 0:
-        zeros = zeros + 1
     if n == 1:
-        ones = ones + 1
+        if cr0 > lr0:
+            lr0 = cr0
+        cr0 = 0
+        cr1 = cr1 + 1
+    elif n == 0:
+        if cr1 > lr1:
+            lr1 = cr1
+        cr1 = 0
+        cr0 = cr0 + 1
+    else:
+        if cr1 > lr1:
+            lr1 = cr1
+        if cr0 > lr0:
+            lr0 = cr0
+        cr0 = 0
+        cr1 = 0
+if cr1 > lr1:
+    lr1 = cr1
+if cr0 > lr0:
+    lr0 = cr0
+    
 
-if ones > zeros:
-    print ("Longest run was ones with length: ", ones)
-if zeros > ones:
-    print ("Longest run was zeros with length: ", zeros)
-if zeros == ones:
-    print ("Equal longest run of ones and zeros with length: ", ones)
+
+
+
+if lr1 > lr0:
+    print ("Longest run was ones with length: ", lr1)
+if lr0 > lr1:
+    print ("Longest run was zeros with length: ", lr0)
+if lr0 == lr1:
+    print ("Equal longest run of ones and zeros with length: ", lr1)
            
