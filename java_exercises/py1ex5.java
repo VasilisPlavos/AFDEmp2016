5ed9b982501158162b38d7d5da8ed6220dc21528
PlavosVasilis <vplavos@gmail.com>
2016-10-16 17:52:35 +0300

    update

---
+++ java_exercises/py1ex5.java
@@ -0,0 +1,10 @@
+public class py1ex5 {
+	public static void main(String[] args) {
+		double sum = 1;
+		System.out.println(1);
+		for (double i = 2; i < 6; i = i + 1){
+			sum = sum + (1/(i));
+			System.out.println(sum);
+		}
+	}
+}
